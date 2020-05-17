54056e5c53132ff043ed1cbf0a0d568905295a36
Thomas Neitmann <th.neitmann@gmail.com>
2020-05-17 17:33:25 +0200

    Document `get_repo_star_history()`

--- R/get_stars.R
+++ R/get_stars.R
@@ -1,3 +1,30 @@
+#' Retrieve GitHub Repo Star Hisotry
+#'
+#' Retrieve the star history of any GitHub repository
+#'
+#' @param repo `character`. Repository name(s) in the form `user/reponame`
+#' @param pkg `character`. Name of an `R` package.
+#'
+#' @details
+#' `get_pkg_star_history()` is a shortcut for retrieving the star history of an
+#' `R` package. The function tries to find the GitHub repository of the package.
+#' If it succeeds it continues calling `get_repo_star_history()`. If it fails either
+#' a warning or an error is thrown depending on whether a GitHub repo couldn't be
+#' found for some or all `pkg`, respectively.
+#'
+#' @return
+#' An object of class `c("ghstars_history_tbl", "data.frame")` with 5 columns:
+#' * `repo`: Name of the repository
+#' * `date`: Date repository was starred
+#' * `day`: Number of days since first star
+#' * `stars`: Number of stars given at the current `date`
+#' * `cumulative_stars`: Cumulative number of stars up to current `date`
+#'
+#' @author Thomas Neitmann
+#'
+#' @examples
+#' get_repo_star_history("thomas-neitmann/mdthemes")
+#'
 #' @export
 get_repo_star_history <- function(repo) {
   if (length(repo) == 1L) {
@@ -5,6 +32,13 @@ get_repo_star_history <- function(repo) {
   }
   ghstars_list <- lapply(repo, get_repo_star_history_single)
   do.call(rbind, ghstars_list)
+}
+
+#' @rdname get_repo_star_history
+#' @export
+get_pkg_star_history <- function(pkg) {
+  repo <- get_pkg_repo(pkg)
+  get_repo_star_history(repo)
 }
 
 get_repo_star_history_single <- function(repo) {
@@ -30,12 +64,6 @@ get_repo_star_history_single <- function(repo) {
     ),
     class = c("ghstars_history_tbl", "data.frame")
   )
-}
-
-#' @export
-get_pkg_star_history <- function(pkg) {
-  repo <- get_pkg_repo(pkg)
-  get_repo_star_history(repo)
 }
 
 #' @export
