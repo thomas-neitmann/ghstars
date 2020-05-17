5a27d19f26c126bfba76467e36d3663a27bb4805
Thomas Neitmann <th.neitmann@gmail.com>
2020-05-17 13:55:21 +0200

    Remove trailing slash from `repo` if present

--- R/get_pkg_repo.R
+++ R/get_pkg_repo.R
@@ -21,6 +21,10 @@ get_pkg_repo_single <- function(pkg) {
 
   match <- gregexpr("github.com/", gh_url)[[1L]]
   repo <- substr(gh_url, match + attr(match, "match.length"), nchar(gh_url))
+  # Remove trailing slash if present
+  if (substr(repo, nchar(repo), nchar(repo)) == "/") {
+    repo <- substr(repo, 1L, nchar(repo)-1L)
+  }
   names(repo) <- pkg
   repo
 }
